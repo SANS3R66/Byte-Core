57794bdf04c01d0272cf5dd41bdbb05042729a97
SanTer <67973452+SANS3R66@users.noreply.github.com>
2024-12-14 18:14:40 +0300

    спиздил все у тейсла надеюсь он не обидется

--- src/Core/Stream/ByteStream.ts
+++ src/Core/Stream/ByteStream.ts
@@ -1,150 +1,215 @@
-// TODO: datareference methods (later)
-
-import { Logger } from "../Logger"
+// https://github.com/tailsjs/nodebrawl-core/blob/main/ByteStream/index.js
 
 export class ByteStream {
-    private buffer: Buffer = Buffer.alloc(0)
-    private offset: number = 0
-    private readonly logger: Logger = new Logger("ByteStream")
+    public buffer: Buffer;
+    public length: number;
+    public offset: number;
+    public bitOffset: number;
 
-    constructor() { }
-
-    public getOffset(): number {
-        return this.offset
-    }
-
-    public seek(n: number) {
-        this.offset = n
-    }
-
-    public writeInt(n: number = 0) {
-        this.expandBuffer(4)
-        if (n < -2147483648) {
-            this.logger.logWarn("Number is less than -2147483648!")
-            this.buffer.writeInt32BE(0, this.offset)
-        } else if (n > 2147483647) {
-            this.logger.logWarn("Number is more than 2147483647!")
-            this.buffer.writeInt32BE(0, this.offset)
-        } else {
-            this.buffer.writeInt32BE(n, this.offset)
-        }
-
-        this.offset += 4
+    constructor() {
+        this.buffer = Buffer.alloc(0);
+        this.length = 0;
+        this.offset = 0;
+        this.bitOffset = 0;
     }
 
     public readInt(): number {
-        const retval = this.buffer.readInt32BE(this.offset)
-        this.offset += 4
-        return retval
+        this.bitOffset = 0;
+        return (
+            (this.buffer[this.offset++] << 24 |
+                (this.buffer[this.offset++] << 16 |
+                    (this.buffer[this.offset++] << 8 |
+                        this.buffer[this.offset++])))
+        );
     }
 
-    public writeShort(n: number) {
-        this.expandBuffer(2)
-        if (n < -32768) {
-            this.logger.logWarn("Short is less than -32768!")
-            this.buffer.writeInt16BE(0, this.offset)
-        } else if (n > 32767) {
-            this.logger.logWarn("Short is more than -32767!")
-            this.buffer.writeInt16BE(0, this.offset)
-        } else {
-            this.buffer.writeInt16BE(n, this.offset)
-        }
-        this.offset += 2
+    public skip(len: number) {
+        this.bitOffset += len;
     }
 
     public readShort(): number {
-        const retval = this.buffer.readInt16BE(this.offset)
-        this.offset += 2
-        return retval
+        this.bitOffset = 0;
+        return (this.buffer[this.offset++] << 8 | this.buffer[this.offset++]);
     }
 
-    public writeString(str: string | null = null) {
-        if (str) {
-            this.writeInt(str.length)
-            this.expandBuffer(str.length)
-            this.buffer.write(str, this.offset, 'utf8')
-            this.offset += str.length
-        } else {
-            this.writeInt(-1)
+    public writeShort(value: number) {
+        this.bitOffset = 0;
+        this.ensureCapacity(2);
+        this.buffer[this.offset++] = (value >> 8);
+        this.buffer[this.offset++] = (value);
+    }
+
+    public writeInt(value: number = 0) {
+        this.bitOffset = 0;
+        this.ensureCapacity(4);
+        this.buffer[this.offset++] = (value >> 24);
+        this.buffer[this.offset++] = (value >> 16);
+        this.buffer[this.offset++] = (value >> 8);
+        this.buffer[this.offset++] = (value);
+    }
+
+    public writeString(value: string | undefined = undefined) {
+        if (value == null || value.length > 90000) {
+            this.writeInt(-1);
+            return;
         }
+
+        const buf = Buffer.from(value, 'utf8');
+        this.writeInt(buf.length);
+        this.buffer = Buffer.concat([this.buffer, buf]);
+        this.offset += buf.length;
     }
 
     public readString(): string {
-        const len = this.readInt()
-        const retval = this.buffer.toString('utf8', this.offset, this.offset + len)
-        this.offset += len
-        return retval
+        const length = this.readInt();
+
+        if (length > 0 && length < 90000) {
+            const stringBytes = this.buffer.slice(this.offset, this.offset + length);
+            const string = stringBytes.toString('utf8');
+            this.offset += length;
+            return string;
+        }
+        return '';
     }
 
-    public writeByte(byte: number): void {
-        this.buffer = Buffer.concat([this.buffer, Buffer.from([byte])]);
-        this.offset++
+    public readDataReference(): number[] {
+        const a1 = this.readVInt();
+        return [a1, a1 == 0 ? 0 : this.readVInt()];
     }
 
-    public readByte(): number {
-        const retval = this.buffer[this.offset]
-        this.offset++
-        return retval
-    }
-
-    // TODO: fix? writeVInt() & readVInt
-
-    public writeVInt(n: number = 0, rotate = true): void {
-        let v1 = (((n >> 25) & 0x40) | (n & 0x3F)),
-            v2 = ((n ^ (n >> 31)) >> 6), v3
-
-        n >>= 6;
-        if (v2 == 0) {
-            this.writeByte(v1);
+    public writeDataReference(value1: number, value2: number) {
+        if (value1 < 1) {
+            this.writeVInt(0);
         } else {
-            this.writeByte(v1 | 0x80);
-            v2 >>= 7;
-            v3 = 0;
-            if (v2 > 0) {
-                v3 = 0x80;
-            }
-            this.writeByte((n & 0x7F) | v3);
-            n >>= 7;
-            while (v2 != 0) {
-                v2 >>= 7;
-                v3 = 0;
-                if (v2 > 0) {
-                    v3 = 0x80;
-                }
-                this.writeByte((n & 0x7F) | v3);
-                n >>= 7;
-            }
+            this.writeVInt(value1);
+            this.writeVInt(value2);
         }
     }
 
-    public readVInt(rotate = true): number {
-        let result = 0;
-        let shift = 0;
+    public writeVInt(value: number = 0) {
+        this.bitOffset = 0;
+        let temp = (value >> 25) & 0x40;
 
-        while (true) {
-            let byte = this.readByte();
+        let flipped = value ^ (value >> 31);
 
-            if (rotate) {
-                const lsb = (byte & 0x20) >> 5;
-                const msb = (byte & 0x80) >> 7;
-                byte = (byte & 0x1F) | (msb << 7) | (lsb << 6);
-                rotate = false;
+        temp |= value & 0x3F;
+
+        value >>= 6;
+        flipped >>= 6;
+
+        if (flipped === 0) {
+            this.writeByte(temp);
+            return 0;
+        }
+
+        this.writeByte(temp | 0x80);
+
+        flipped >>= 7;
+        let r = 0;
+
+        if (flipped) { r = 0x80; }
+
+        this.writeByte((value & 0x7F) | r);
+
+        value >>= 7;
+
+        while (flipped !== 0) {
+            flipped >>= 7;
+            r = 0;
+            if (flipped) { r = 0x80; }
+            this.writeByte((value & 0x7F) | r);
+            value >>= 7;
+        }
+    }
+
+    public readVInt(): number {
+        let result = 0,
+            shift = 0,
+            s = 0,
+            a1 = 0,
+            a2 = 0;
+        do {
+            let byte = this.buffer[this.offset++];
+            if (shift === 0) {
+                a1 = (byte & 0x40) >> 6;
+                a2 = (byte & 0x80) >> 7;
+                s = (byte << 1) & ~0x181;
+                byte = s | (a2 << 7) | a1;
             }
-
-            result |= (byte & 0x7F) << shift;
+            result |= (byte & 0x7f) << shift;
             shift += 7;
+            if (!(byte & 0x80)) { break; }
+        } while (true);
 
-            if ((byte & 0x80) === 0) {
-                break;
-            }
-        }
-
-        return (result >>> 1) ^ (-(result & 1));
+        return (result >> 1) ^ (-(result & 1));
     }
 
-    public expandBuffer(n: number) {
-        const newBuffer = Buffer.alloc(n)
-        this.buffer = Buffer.concat([this.buffer, newBuffer])
+    public writeBoolean(value: boolean) {
+        if (this.bitOffset === 0) {
+            this.ensureCapacity(1);
+            this.buffer[this.offset++] = 0;
+        }
+
+        if (value) { this.buffer[this.offset - 1] |= (1 << this.bitOffset); }
+
+        this.bitOffset = (this.bitOffset + 1) & 7;
+    }
+
+    public readBoolean(): boolean {
+        return this.readVInt() >= 1;
+    }
+
+    public writeStringReference = this.writeString;
+
+    public writeLongLong(value: number) {
+        this.writeInt(value >> 32);
+        this.writeInt(value);
+    }
+
+    public writeLogicLong(value1: number, value2: number) {
+        this.writeVInt(value1);
+        this.writeVInt(value2);
+    }
+
+    public readLogicLong(): number[] {
+        return [this.readVInt(), this.readVInt()];
+    }
+
+    public writeLong(value1: number, value2: number) {
+        this.writeInt(value1);
+        this.writeInt(value2);
+    }
+
+    public readLong(): number[] {
+        return [this.readInt(), this.readInt()];
+    }
+
+    public writeByte(value: number) {
+        this.bitOffset = 0;
+        this.ensureCapacity(1);
+        this.buffer[this.offset++] = value;
+    }
+
+    public writeBytes(buffer: Buffer) {
+        const length = buffer.length;
+
+        if (buffer != null) {
+            this.writeInt(length);
+            this.buffer = Buffer.concat([this.buffer, buffer]);
+            this.offset += length;
+            return;
+        }
+
+        this.writeInt(-1);
+    }
+
+    public ensureCapacity(capacity: number) {
+        const bufferLength = this.buffer.length;
+
+        if (this.offset + capacity > bufferLength) {
+            const tmpBuffer = Buffer.alloc(capacity);
+            this.buffer = Buffer.concat([this.buffer, tmpBuffer]);
+        }
     }
 
     public reset() {
